bbc576fdb9d5f25eef98687ce30210a3137f5385
Hawkart <hawkart@rambler.ru>
2018-10-24 16:37:28 +0300

    Echo full path

--- resources/assets/js/bootstrap.js
+++ resources/assets/js/bootstrap.js
@@ -53,7 +53,7 @@ window.io = require('socket.io-client');
 
 window.Echo = new Echo({
     broadcaster: 'socket.io',
-    host: window.location.hostname + ':6004'//+window.Laravel.echoPort
+    host: 'https://yesl.tv:6004'//window.location.hostname + ":"+window.Laravel.echoPort
 });
 
 /**
