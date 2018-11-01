8472bfc9a3e9d3417cdff39085d4f3c34dbc0fce
Hawkart <hawkart@rambler.ru>
2018-11-01 15:49:46 +0300

    Production js bugfix

--- webpack.mix.js
+++ webpack.mix.js
@@ -1,28 +1,10 @@
-const mix = require('laravel-mix')
-//const { BundleAnalyzerPlugin } = require('webpack-bundle-analyzer')
-
-mix.config.vue.esModule = true
-
+let mix = require('laravel-mix');
 mix
     .js('resources/assets/js/app.js', 'public/js')
     .sass('resources/assets/sass/app.scss', 'public/css')
-
     .sourceMaps()
     .disableNotifications()
 
 if (mix.inProduction) {
     mix.version()
 }
-
-mix.webpackConfig({
-    plugins: [
-        //new BundleAnalyzerPlugin()
-    ],
-    resolve: {
-        extensions: ['.js', '.json', '.vue'],
-    },
-    output: {
-        publicPath: mix.config.hmr ? '//localhost:8080' : '/',
-        jsonpFunction: 'webpackJsonp'
-    }
-})
