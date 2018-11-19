092bfadd1bf511057c56ec31df40fbb5aa937380
Hawkart <hawkart@rambler.ru>
2018-11-19 12:45:09 +0300

    Google adsense

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -10,6 +10,15 @@
             @include('feeds._menu')
 
             <div class="col col-xl-3 order-xl-3 col-lg-6 order-lg-3 col-md-6 col-sm-12 col-12">
+
+                <script async src="//pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"></script>
+                <script>
+                    (adsbygoogle = window.adsbygoogle || []).push({
+                        google_ad_client: "ca-pub-4709211777366339",
+                        enable_page_level_ads: true
+                    });
+                </script>
+
                 <friend-possible-widget/>
             </div>
         </div>
